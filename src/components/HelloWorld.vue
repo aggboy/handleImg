856c462341f1e2d5b65bef1a43638b5403059fd8
zzq <a576810623@qq.com>
2020-03-27 16:25:02 +0800

    初次提交

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,175 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div>
+    <img @click="startPhoto"
+         :src="bigSrcObj.url"
+         alt="">
+    <el-dialog title=""
+               :visible.sync="showPhotoDialog"
+               :close-on-click-modal='false'
+               width="30%"
+               center>
+      <!-- 滚轮的照片 如果不需要 把他整个img删除即可 -->
+      <div class="one-dialog-pic"
+           :style="{'width':(style.mywidth+'px'),'height':(style.myheight+'px'),'overflow':'hidden'}">
+
+        <img v-show="style.display"
+             :src="bigSrcObj.url"
+             :style="{'width':'100%;','height':'100%','transform':'scale'+'('+style.scale+')','position':'relative','left':style.left,'top':style.top}">
+      </div>
+
+      <!-- 真实的照片 -->
+      <img id="wheelImg"
+           style="opacity:0; width:100%;height:auto"
+           :src="bigSrcObj.url"
+           @mouseenter="openWheel"
+           @mouseleave="closeWheel"
+           @mousedown="openDrag">
+
+      <span slot="footer"
+            class="dialog-footer">
+        <el-button type='danger'
+                   @click="openTwoRefuse">取消</el-button>
+        <el-button type='primary'
+                   @click="handleOneAgree">确认</el-button>
+      </span>
+    </el-dialog>
   </div>
+
 </template>
 
 <script>
 export default {
   name: 'HelloWorld',
-  props: {
-    msg: String
+  data () {
+    return {
+      bigSrcObj: { url: 'http://img1.imgtn.bdimg.com/it/u=4018557288,1217151095&fm=26&gp=0.jpg' },
+      showPhotoDialog: false,
+      style: {
+        mywidth: 400,
+        myheight: 400,
+        display: false,
+        scale: 1,
+        left: null,
+        top: null
+      }
+    }
+  },
+  methods: {
+    startPhoto () {
+      this.showPhotoDialog = true
+      this._hanleInitRollImg()
+      this._handleInitDrag()
+    },
+    openWheel () {
+      this._initMouseWheel()
+    },
+    closeWheel () {
+      this.stopMouseWheel()
+    },
+    // 鼠标滚轮操作
+    rollImg (e) {
+      this._stopScrollFunc()
+      if (e.wheelDelta) { // IE/Opera/Chrome
+        let t1 = e.wheelDelta
+        if (t1 > 0) {
+          this.style.scale = this.style.scale + 0.1
+        } else {
+          this.style.scale = this.style.scale - 0.1
+        }
+      } else if (e.detail) { // Firefox
+        let t2 = e.detail
+        if (t2 > 0) {
+          this.style.scale = this.style.scale + 0.1
+        } else {
+          this.style.scale = this.style.scale - 0.1
+        }
+      }
+      return false
+    },
+    // 鼠标 双击事件 开启拖拽模式
+    openDrag (e) {
+      let that = this
+      let odiv = e.target      // 获取目标元素
+      // 阻止默认事件的方法,如果不阻止默认事件onmouseup会无法触发
+      e.preventDefault()
+      // 算出鼠标相对元素的位置
+      let disX = e.clientX - odiv.offsetLeft
+      let disY = e.clientY - odiv.offsetTop
+      // 计算两边坐标
+      document.onmousemove = function (e) {
+        // 鼠标按下并移动的事件
+        // 用鼠标的位置减去鼠标相对元素的位置，得到元素的位置
+        // 因为img居中对齐，所以还要减去目标元素距离body的偏移量
+        let left = (e.clientX - disX - odiv.offsetLeft) * that.style.scale
+        let top = (e.clientY - disY) * that.style.scale
+        // 移动当前元素
+        that.style.left = left + 'px'
+        that.style.top = top + 'px'
+      }
+      // 鼠标停止移动时，事件移除
+      document.onmouseup = function () {
+        document.onmousemove = null
+        document.onmouseup = null
+      }
+    },
+    _handleInitDrag () {
+      this.style.left = 0
+      this.style.top = 0
+    },
+    _hanleInitRollImg () {
+      this.style.display = false
+      this.style.scale = 1
+      setTimeout(() => {
+        let rollImg = document.getElementById('wheelImg')
+        this.style.mywidth = rollImg.width - 30
+        this.style.myheight = rollImg.height - 30
+        this.style.display = true
+      }, 500)
+    },
+    _initMouseWheel () {
+      // window.addEventListener('mousewheel', this.rollImg, { passive: false })
+      // // firefox
+      // window.addEventListener('DOMMouseScroll', this.rollImg, { passive: false })
+      document.getElementById('wheelImg').addEventListener('DOMMouseScroll', this.rollImg, { passive: false })
+      document.getElementById('wheelImg').addEventListener('mousewheel', this.rollImg, { passive: false })
+    },
+    stopMouseWheel () {
+      document.getElementById('wheelImg').removeEventListener('DOMMouseScroll', this.rollImg)
+      document.getElementById('wheelImg').removeEventListener('mousewheel', this.rollImg)
+    },
+    _stopScrollFunc (evt) {
+      evt = evt || window.event
+      if (evt.preventDefault) {
+        // Firefox
+        evt.preventDefault()
+        evt.stopPropagation()
+      } else {
+        // IE
+        evt.cancelBubble = true
+        evt.returnValue = false
+      }
+      return false
+    },
+    openTwoRefuse () {
+      this.showPhotoDialog = true
+    },
+    handleOneAgree () {
+      this.showPhotoDialog = true
+    }
+  },
+  bigSrcObj: {
+    url: 'https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=4018557288,1217151095&fm=26&gp=0.jpg'
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+  .one-dialog-pic {
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    left: 50%;
+    transform: translate(-50%);
+  }
 </style>
